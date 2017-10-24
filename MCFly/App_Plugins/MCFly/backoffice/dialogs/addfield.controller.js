95a8118aa609b471c35303d3ccd85c6bf90624ac
LAPTOP-JSAC0SHM\Tim Geyssens <tim@nibble.be>
2017-10-24 21:48:37 +0200

    Make it possible to setup options if fieldtype supports them

--- MCFly/App_Plugins/MCFly/backoffice/dialogs/addfield.controller.js
+++ MCFly/App_Plugins/MCFly/backoffice/dialogs/addfield.controller.js
@@ -2,16 +2,31 @@
     .controller("MCFly.AddFieldController",
     function ($scope, mcFlyResource, dialogService) {
         $scope.newField = {};
+        $scope.newField.options = [];
         $scope.newField.regEx = "";
         $scope.newField.placeholder = "";
         $scope.newField.formId = $scope.dialogData.form.Id
         $scope.newField.fieldTypeName = "TextBox";
         $scope.currentSupportsPlaceholder = true;
+        $scope.currentSupportsOptions = false;
 
         mcFlyResource.getFormBuilderData().then(function (resp) {
             $scope.fieldtypes = resp.data.FieldTypes;
         });
+        $scope.removeOption = function (option)
+        {
+            $scope.newField.options.splice($scope.newField.options.indexOf(option), 1);
+        }
+        $scope.addOption = function () {
+            if ($scope.newField.$newOptionValue && $scope.newField.$newOptionValue != "") {
 
+                var newOption = {
+                    'value': $scope.newField.$newOptionValue
+                }
+                $scope.newField.options.push(newOption);
+                $scope.newField.$newOptionValue = "";
+            }
+        }
         $scope.addField = function () {
            
             $scope.newField.$locked = true;
@@ -21,16 +36,16 @@
             $scope.submit($scope.dialogData.form);
         }
 
-        $scope.supportsPlaceholder = function (fieldTypeName) {
-            
-            $scope.currentSupportsPlaceholder = _.findWhere($scope.fieldtypes, { Name: fieldTypeName }).SupportsPlaceholder;
+        $scope.checkSupports = function (fieldTypeName) {
+
+            var current = _.findWhere($scope.fieldtypes, { Name: fieldTypeName });
+            console.log(current);
+            $scope.currentSupportsPlaceholder = current.SupportsPlaceholder;
+            $scope.currentSupportsOptions = current.SupportsOptions;
 
         }
 
-        $scope.supportsOptions = function (fieldTypeName) {
-            return _.findWhere($scope.fieldtypes, { Name: fieldTypeName }).SupportsOptions;
-
-        }
+       
         $scope.getSafeAlias = function (value) {
 
             mcFlyResource.getSafeAlias(value).then(function (resp) {
